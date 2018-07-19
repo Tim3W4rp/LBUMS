32676785009afaa83066d98c32a7d2dd04b0a64e
Martin Jordán <martin@jordan.cz>
2018-07-19 16:41:01 +0200

    add button

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+class Button = () => {
+  return (
+    <Text>Click me!!!</Text>
+  );
+};
+
+export default Button;
